2e7a19a8a7a1986d687d6ee9c7139d33f6a3c94f
BONEY <boney003.upadhyay@gmail.com>
2021-02-12 17:24:37 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Game from "./Components/Game";
+import Board from "./Components/Board";
+import Square from "./Components/Square";
 
-function App() {
+const  App=()=>{
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game />
+      <Board />
+      <Square />
     </div>
   );
 }
---
+++ src/Components/Board.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Square from "./Square";
+
+
+const Board=()=>{
+<Square/>
+};
+
+export default Board;
